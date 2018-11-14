ee581f66bf3fb92bfca3a1bca3f2d1437be61ef0
John Restrepo <jfrestre@gmail.com>
2018-11-13 19:10:21 -0500

    Added script to crop images Modified process_images script to produce plots for energy vs various parameters

---
+++ crop_images.py
@@ -0,0 +1,52 @@
+# Script to crop the images centered on a computed point
+
+import os, glob
+import cv2 as cv
+import numpy as np
+
+def main():
+    target_folder = "D:/Research/ModeTransformation/Data/05_10_2018/"
+    file_mask = "I*A*P*"              #"I0000A*P040*"
+    file_extension = ".png"
+    results_folder = "D:/Research/ModeTransformation/Data/05_10_2018/cropped"
+    height, width = (512, 640)
+    crop_height, crop_width = (160, 160)
+
+    images_list = glob.glob(os.path.join(target_folder, file_mask+file_extension))
+    print("Processing the following list of images: ")
+    print([[x] for x in images_list])
+
+    for itr, item in enumerate(images_list):
+        print("Processing image: ", item)
+        image = cv.imread(item, cv.IMREAD_UNCHANGED)
+        ret2, binary = cv.threshold(image, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+        binary_array = np.array(binary)
+        array = np.array(image)
+
+        # Pad arrays to avoid border inaccuracies
+        array = np.pad(array, ((128, 128), (160, 160)), 'constant', constant_values=0)
+        binary_array = np.pad(binary_array, ((128, 128), (160, 160)), 'constant', constant_values=0)
+
+        yc, xc = compute_centroid(binary_array)
+        array_cropped = array[int(xc-crop_height*0.5):int(xc+crop_height*0.5),
+                              int(yc - crop_height * 0.5):int(yc + crop_height * 0.5)]
+        current_file = os.path.splitext(os.path.basename(item))[0]
+        print("Writting image to file: ", os.path.join(results_folder, current_file+"_crop"+file_extension))
+        cv.imwrite(os.path.join(results_folder, current_file+"_crop"+file_extension), array_cropped)
+        # cv.waitKey(0)
+
+    print("Done....goodbye")
+
+def compute_centroid(array):
+    """"Compute centroid of the array"""
+    X, Y = np.meshgrid(np.linspace(0,array.shape[1]-1, array.shape[1]), np.linspace(0,array.shape[0]-1, array.shape[0]))
+    X += 0.5
+    Y += 0.5
+    centroid_x = np.sum(array * X) / np.sum(array)
+    centroid_y = np.sum(array * Y) / np.sum(array)
+    return centroid_x, centroid_y
+
+
+if __name__ == "__main__":
+    main()
+
--- process_images.py
+++ process_images.py
@@ -1,44 +1,43 @@
-# Script for processing the images
+# Script for processing the images.
+# Compute plots of energy for varying parameters
 
 import os, glob
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 def main():
-    target_folder = "D:/Research/ModeTransformation/Data/05_10_2018/"
-    filename_mask = "I0000A*P040*.png"
-    height, width = (512, 640)
-    crop_height, crop_width = (300, 300)
+    target_folder = "D:/Research/ModeTransformation/Data/05_10_2018/cropped"
+    file_mask = "I*A320P130*"
+    file_extension = ".png"
 
-    images_list = glob.glob(os.path.join(target_folder, filename_mask))
+    images_list = glob.glob(os.path.join(target_folder, file_mask))
 
-    projection = np.zeros((height, width))
-    centroid_list = []
-
+    energy = []
     for itr, item in enumerate(images_list):
-        print("Reading image: ", images_list[itr])
+        print("Reading image: ", item)
         image = cv2.imread(item, cv2.IMREAD_UNCHANGED)
-        ret2, binary = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        projection = projection + np.array(binary)
-        centroid_list.append(compute_centroid(binary))
+        array = np.array(image)
+        energy.append(np.sum(array))
 
+    # Normalize the energy
+    energy /= np.max(energy)
+    # t = range(180, 370, 10)                          #Analyzer plot
+    t = [0, 280, 580, 820, 1030, 1210]             # Analyzer plot
 
-    projection_centroid = compute_centroid(projection)
-    projection = projection - np.min(projection)
-    projection = projection / np.max(projection)
+    fig, ax = plt.subplots()
+    ax.plot(t, energy, "r--")
 
-    cv2.imshow('Projection', projection)
-    # cv2.circle(projection, (int(projection_centroid[0]), int(projection_centroid[1])), 2, (0,255,255), 1, -1)
-    # cv2.imshow("Centroid", projection)
-    print(projection_centroid)
-    print(compute_centroid(np.ones((640,512))))
+    ax.set(xlabel='Current(A)', ylabel='Energy (A.U.)',
+           title='')
+    plt.xticks(t)
+    ax.grid()
 
-    for itr, item in enumerate(centroid_list):
-        cv2.circle(projection, (int(item[0]), int(item[1])), 2, (0, 255, 255), 1, -1)
-    cv2.imshow("All centroids", projection)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # mng = plt.get_current_fig_manager()
+    # mng.resize(*mng.window.maxsize())
 
+    # fig.savefig(os.path.join(target_folder, file_mask+file_extension))
+    plt.show()
 
 def compute_centroid(array):
     """"Compute centroid of the array"""
